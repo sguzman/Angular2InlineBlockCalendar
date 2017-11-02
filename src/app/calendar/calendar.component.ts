d7a6a92ec5c0364e1c02eeaa20d8007c4466064c
Salvador Guzman <guzmansalv@gmail.com>
2017-11-02 14:36:23 -0700

    added header and body components to calendar component

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
