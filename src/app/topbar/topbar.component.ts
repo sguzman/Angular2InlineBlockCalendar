05d325a211697fe29d7709bef5d23795f71dbe60
Salvador Guzman <guzmansalv@gmail.com>
2017-11-02 16:35:59 -0700

    added logic for month selection

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent implements OnInit {
+  public monthIdx: number = 0;
+  public months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  constructor() { }
+  constructor() {}
+
+  public decre() {
+    this.monthIdx = (this.monthIdx + 1) % this.months.length;
+  }
+
+  public incre() {
+    if (this.monthIdx === 0) {
+      this.monthIdx = this.months.length - 1;
+    } else {
+      this.monthIdx = (this.monthIdx - 1) % this.months.length;
+    }
+  }
 
   ngOnInit() {
   }
