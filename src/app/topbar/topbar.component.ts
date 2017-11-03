1846b8c38e106384cef3adf98e167d46e4c1ee1d
Salvador Guzman <guzmansalv@gmail.com>
2017-11-02 17:20:30 -0700

    use Subject

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {ConnectMonthService} from '../connect-month.service';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
+  providers: [ConnectMonthService]
 })
 export class TopbarComponent implements OnInit {
   public monthIdx: number = 0;
-  public months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  lastClicked: boolean = false;
+  months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  leftRight$ = new Subject<boolean>();
 
-  constructor() {}
+  constructor(private _shareMonth: ConnectMonthService) {
+      this.leftRight$.subscribe(
+        data => {
+          console.log(data);
+          if (data) {
+            this.monthIdx = ++this.monthIdx % this.months.length;
+          } else {
+            this.monthIdx = this.monthIdx < 1? this.months.length - 1: --this.monthIdx;
+          }
+
+          this._shareMonth.publishData(this.monthIdx);
+        }
+      )
+  }
 
   public decre() {
-    this.monthIdx = (this.monthIdx + 1) % this.months.length;
+    this.leftRight$.next(false);
   }
 
   public incre() {
-    if (this.monthIdx === 0) {
-      this.monthIdx = this.months.length - 1;
-    } else {
-      this.monthIdx = (this.monthIdx - 1) % this.months.length;
-    }
+    this.leftRight$.next(true);
   }
 
   ngOnInit() {
